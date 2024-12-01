5d41e71abfb1f12734f771e4ad295beb68f1b76c
Chinmay Senapathi <cksenapathi@utexas.edu>
2024-12-01 12:47:44 -0600

    added models

--- scripts/main.py
+++ scripts/main.py
@@ -8,7 +8,7 @@ def main():
     env = Monitor(CTFEnv(["../images/team1.png", "../images/team2.png"], ["../images/flag1.png", "../images/flag2.png"]), '../logs')
 
     # Training
-    self_play_training(env, save_dir="./models", total_timesteps=5000, self_play_epochs=10)
+    # self_play_training(env, save_dir="./models", total_timesteps=5000, self_play_epochs=10)
 
     opp_model = PPO.load("./models/team1/team1_550000_steps.zip")
     model = PPO.load("./models/team1/team1_540000_steps.zip")
@@ -16,17 +16,18 @@ def main():
 
     metrics = validation(env, num_episodes, model, opp_model)
     # Evaluation
-    # opp_model = PPO.load("./models/team1/team1_550000_steps.zip")
-    # env.set_opponent_policy("learned", opp_model)
-    # model = PPO.load("./models/team1/team1_540000_steps.zip")
+    opp_model = PPO.load("./models/team1/team1_290000_steps.zip")
+    env.set_opponent_policy("learned", opp_model)
+    model = PPO.load("./models/team1/team1_290000_steps.zip")
 
-    # obs, info = env.reset()
+    for _ in range(5):
+        obs, info = env.reset()
 
-    # done = False
-    # while not done:
-    #     act = model.predict(obs)[0]
-    #     obs, reward, done, trunc, info = env.step(act)
-    #     env.render()
+        done = False
+        while not done:
+            act = model.predict(obs)[0]
+            obs, reward, done, trunc, info = env.step(act)
+            env.render()
 
 if __name__ == "__main__":
     main()
